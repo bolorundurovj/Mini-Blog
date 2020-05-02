fb3cda816b21cfa8eddf9b8b9bff8087ea8230b4
bolorundurovj <valiantjoshuabolorunduro@gmail.com>
2020-05-02 17:22:16 +0100

    Getting image to client

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -29,6 +29,9 @@ export class PostCreateComponent implements OnInit {
       }),
       'content': new FormControl(null, {
         validators: [Validators.required]
+      }),
+      image: new FormControl(null, {
+        validators: [Validators.required]
       })
     });
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
@@ -54,6 +57,14 @@ export class PostCreateComponent implements OnInit {
         this.postId = null;
       }
     });
+  }
+
+  onImagePicked(event: Event){
+    const file = (event.target as HTMLInputElement).files[0];
+    this.form.patchValue({image: file});
+    this.form.get('image').updateValueAndValidity();
+    console.log(file);
+    console.log(this.form);
   }
 
   onSavePost(){
