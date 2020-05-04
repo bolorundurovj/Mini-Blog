8455f26b94babc525fa9974548c1beefce590e8f
bolorundurovj <valiantjoshuabolorunduro@gmail.com>
2020-05-04 22:07:25 +0100

    Update mime-type.validator.ts

--- src/app/posts/post-create/mime-type.validator.ts
+++ src/app/posts/post-create/mime-type.validator.ts
@@ -1,7 +1,12 @@
 import { AbstractControl } from '@angular/forms';
-import { Observable } from 'rxjs';
+import { Observable, Observer } from 'rxjs';
 
 export const mimeType = (control: AbstractControl): Promise<{[key: string]: any}> | Observable<{[key: string]: any}> => {
   const file = control.value as File;
   const fileReader = new FileReader();
+  const frObs = Observable.create((observer: Observer) => {
+    fileReader.addEventListener("loadend", () => {
+      
+    });
+  });
 };
