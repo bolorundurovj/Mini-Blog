bc99c7c236b647477b2f06e94f8091eec49fa60f
bolorundurovj <valiantjoshuabolorunduro@gmail.com>
2020-05-04 22:20:03 +0100

    Update mime-type.validator.ts

--- src/app/posts/post-create/mime-type.validator.ts
+++ src/app/posts/post-create/mime-type.validator.ts
@@ -11,6 +11,21 @@ export const mimeType = (control: AbstractControl): Promise<{[key: string]: any}
       for(let i=0; i<arr.length; i++) {
         header += arr[i].toString(16);
       }
+      switch(header){
+        case "89504e47":
+          isValid = true;
+          break;
+        case "ffd8ffe0":
+        case "ffd8ffe1":
+        case "ffd8ffe2":
+        case "ffd8ffe3":
+        case "ffd8ffe8":
+          isValid = true;
+          break;
+        default:
+          isValid = false;
+          break;
+      }
     });
     fileReader.readAsArrayBuffer(file);
   });
