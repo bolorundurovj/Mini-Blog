9de0ba6bf909b1f3f6d2ed7bcde2bec8e7b9b232
bolorundurovj <valiantjoshuabolorunduro@gmail.com>
2020-05-07 13:11:52 +0100

    update

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -10,9 +10,8 @@ import { mimeType } from './mime-type.validator';
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
-  styleUrls: ['./post-create.component.css']
+  styleUrls: ['./post-create.component.css'],
 })
-
 export class PostCreateComponent implements OnInit {
   enteredTitle = '';
   enteredContent = '';
@@ -23,49 +22,53 @@ export class PostCreateComponent implements OnInit {
   private mode = 'create';
   private postId: string;
 
-  constructor (public postsService: PostsService, public route: ActivatedRoute) {}
+  constructor(
+    public postsService: PostsService,
+    public route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
     this.form = new FormGroup({
-      'title': new FormControl(null, {
-        validators: [Validators.required, Validators.minLength(3)]
+      title: new FormControl(null, {
+        validators: [Validators.required, Validators.minLength(3)],
       }),
-      'content': new FormControl(null, {
-        validators: [Validators.required]
+      content: new FormControl(null, {
+        validators: [Validators.required],
       }),
-      'image': new FormControl(null, {
-        validators: [Validators.required], asyncValidators: [mimeType]
-      })
+      image: new FormControl(null, {
+        validators: [Validators.required],
+        asyncValidators: [mimeType],
+      }),
     });
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
-      if(paramMap.has('postId')) {
+      if (paramMap.has('postId')) {
         this.mode = 'edit';
         this.postId = paramMap.get('postId');
         this.isLoading = true;
-        this.postsService.getPost(this.postId).subscribe(postData => {
+        this.postsService.getPost(this.postId).subscribe((postData) => {
           this.isLoading = false;
           this.post = {
             id: postData._id,
             title: postData.title,
             content: postData.content,
-            imagePath: null
+            imagePath: postData.imagePath,
           };
           this.form.setValue({
-            'title': this.post.title,
-            'content': this.post.content
+            title: this.post.title,
+            content: this.post.content,
+            image: this.post.imagePath,
           });
         });
-      }
-      else{
+      } else {
         this.mode = 'create';
         this.postId = null;
       }
     });
   }
 
-  onImagePicked(event: Event){
+  onImagePicked(event: Event) {
     const file = (event.target as HTMLInputElement).files[0];
-    this.form.patchValue({image: file});
+    this.form.patchValue({ image: file });
     this.form.get('image').updateValueAndValidity();
     const reader = new FileReader();
     reader.onload = () => {
@@ -74,18 +77,26 @@ export class PostCreateComponent implements OnInit {
     reader.readAsDataURL(file);
   }
 
-  onSavePost(){
-    if(this.form.invalid){
+  onSavePost() {
+    if (this.form.invalid) {
       return;
     }
     this.isLoading = true;
-    if(this.mode === 'create'){
-      this.postsService.addPost(this.form.value.title, this.form.value.content, this.form.value.image);
-    }
-    else{
-      this.postsService.updatePost(this.postId, this.form.value.title, this.form.value.content);
+    if (this.mode === 'create') {
+      this.postsService.addPost(
+        this.form.value.title,
+        this.form.value.content,
+        this.form.value.image
+      );
+    } else {
+      this.postsService.updatePost(
+        this.postId,
+        this.form.value.title,
+        this.form.value.content,
+        this.form.value.image
+      );
     }
 
     this.form.reset();
-   }
+  }
 }
