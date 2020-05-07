1816172c7cf8d4d5d9dd917d8a93f22f159cf90f
bolorundurovj <valiantjoshuabolorunduro@gmail.com>
2020-05-07 13:11:28 +0100

    update

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -74,11 +74,18 @@ router.get('/:id', (req, res, next) => {
 });
 
 //Update Post
-router.put('/:id', (req, res, next) => {
+router.put('/:id',multer({storage: storage}).single("image"), (req, res, next) => {
+  let imagePath = req.body.imagePath;
+  if(req.file){
+    const url = req.protocol + '://' + req.get("host");
+    imagePath = url + "/images/" + req.file.filename
+
+  }
   const post = new Post({
     _id: req.body.id,
     title: req.body.title,
-    content: req.body.content
+    content: req.body.content,
+    imagePath: imagePath
   });
   Post.updateOne({_id: req.params.id}, post).then(result => {
     console.log(result);
