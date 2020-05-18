4be8a8bb0d6cfe08e5c02f2b784978b4bbc0b140
bolorundurovj <valiantjoshuabolorunduro@gmail.com>
2020-05-18 11:22:34 +0100

    Formatted code

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = require("./backend/app");
 const debug = require("debug")("node-angular");
 const http = require("http");
 
-const normalizePort = val => {
+const normalizePort = (val) => {
   var port = parseInt(val, 10);
 
   if (isNaN(port)) {
@@ -18,7 +18,7 @@ const normalizePort = val => {
   return false;
 };
 
-const onError = error => {
+const onError = (error) => {
   if (error.syscall !== "listen") {
     throw error;
   }
@@ -50,4 +50,3 @@ const server = http.createServer(app);
 server.on("error", onError);
 server.on("listening", onListening);
 server.listen(port);
-
