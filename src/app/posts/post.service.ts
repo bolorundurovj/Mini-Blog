b8ff095c1110a6beb8edb06e96d51b1699fe9ca0
bolorundurovj <valiantjoshuabolorunduro@gmail.com>
2020-05-18 02:42:05 +0100

    fetching posts correctly

--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -9,30 +9,31 @@ import { Post } from './post.model';
 @Injectable({ providedIn: 'root' })
 export class PostsService {
   private posts: Post[] = [];
-  private postsUpdated = new Subject<Post[]>();
+  private postsUpdated = new Subject<{posts: Post[], postCount: number}>();
 
   constructor(private http: HttpClient, private router: Router) {}
 
   getPosts(postsPerPage: number, currentPage: number) {
     const queryParams = `?pagesize=${postsPerPage}&page=${currentPage}`
     this.http
-      .get<{ message: string; posts: any }>('http://localhost:3000/api/posts'+ queryParams)
+      .get<{ message: string; posts: any, maxPosts: number }>('http://localhost:3000/api/posts'+ queryParams)
       .pipe(
         map((postData) => {
-          return postData.posts.map((post) => {
+          return {posts: postData.posts.map((post) => {
             return {
               id: post._id,
               title: post.title,
               content: post.content,
               imagePath: post.imagePath,
             };
-          });
+          }),
+          maxPosts: postData.maxPosts};
         })
       )
-      .subscribe((transformedPosts) => {
-        this.posts = transformedPosts;
-        this.postsUpdated.next([...this.posts]);
-        console.log(transformedPosts);
+      .subscribe((transformedPostData) => {
+        this.posts = transformedPostData.posts;
+        this.postsUpdated.next({posts: [...this.posts], postCount: transformedPostData.maxPosts});
+        // console.log(transformedPosts);
       });
   }
 
