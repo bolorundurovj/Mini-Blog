e5348b38389a7457ebcc78bea5b836a7b4bca584
bolorundurovj <valiantjoshuabolorunduro@gmail.com>
2020-05-26 15:46:20 +0100

    sending token to frontend

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -29,6 +29,7 @@ router.post("/signup", (req, res, next) => {
 });
 
 router.post("/login", (req, res, next) => {
+  let fetchedUser;
   User.findOne({ email: req.body.email })
     .then((user) => {
       if (!user) {
@@ -36,6 +37,7 @@ router.post("/login", (req, res, next) => {
           message: "Auth failed",
         });
       }
+      fetchedUser = user;
       return bcrypt.compare(req.body.password, user.password);
     })
     .then((result) => {
@@ -45,10 +47,13 @@ router.post("/login", (req, res, next) => {
         });
       }
       const token = jwt.sign(
-        { email: user.email, userId: user._id },
+        { email: fetchedUser.email, userId: fetchedUser._id },
         "this_should_be_my_secret_key_but_whatever",
         { expiresIn: "1h" }
       );
+      res.status(200).json({
+        token: token
+      });
     })
     .catch((err) => {
       return res.status(401).json({
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -16,8 +16,16 @@ export class AuthService{
     this.http.post("http://localhost:3000/api/user/signup", authData)
     .subscribe(response => {
       console.log(response);
-
     })
-  }
+  };
+
+  login(email: string, password: string){
+    const authData: AuthData = {email: email, password: password};
+
+    this.http.post("http://localhost:3000/api/user/login", authData)
+    .subscribe(response => {
+      console.log(response);
+    })
+  };
 
 }
