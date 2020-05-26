c2054504226154d258e703f126722faacba865a3
bolorundurovj <valiantjoshuabolorunduro@gmail.com>
2020-05-26 17:48:54 +0100

    authenticating requests with token

--- backend/app.js
+++ backend/app.js
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
 
   res.setHeader("Access-Control-Allow-Headers",
-   "Origin, X-Requested-With, Content-Type, Accept");
+   "Origin, X-Requested-With, Content-Type, Accept, Authorization");
 
   res.setHeader("Access-Control-Allow-Methods",
    "GET, POST, PATCH, DELETE, PUT, OPTIONS");
--- backend/middleware/check-auth.js
+++ backend/middleware/check-auth.js
@@ -4,7 +4,7 @@ module.exports = (req, res, next) => {
   try{
     const token = req.headers.authorization.split(" ")[1];
     jwt.verify(token, "this_should_be_my_secret_key_but_whatever");
-    next(); 
+    next();
   } catch (error) {
     res.status(401).json({message: "Auth failed"});
   }
