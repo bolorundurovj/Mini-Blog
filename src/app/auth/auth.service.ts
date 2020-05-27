97ae02d7a4a31071eed083884924cb61b6c36944
bolorundurovj <valiantjoshuabolorunduro@gmail.com>
2020-05-27 17:09:57 +0100

    improving posts with auth

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@angular/core";
+import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { AuthData } from './auth-data.model';
@@ -6,8 +6,8 @@ import { Subject } from 'rxjs';
 
 // To provide at root level for access
 @Injectable({ providedIn: 'root' })
-
-export class AuthService{
+export class AuthService {
+  private isAuthenticated = false;
   private token: string;
   private authStatusListener = new Subject<boolean>();
 
@@ -17,28 +17,36 @@ export class AuthService{
     return this.token;
   }
 
+  getIsAuth() {
+    return this.isAuthenticated;
+  }
+
   getAuthStatusListener() {
     return this.authStatusListener.asObservable();
   }
 
-  createUser(email: string, password: string){
-    const authData: AuthData = {email: email, password: password};
+  createUser(email: string, password: string) {
+    const authData: AuthData = { email: email, password: password };
 
-    this.http.post("http://localhost:3000/api/user/signup", authData)
-    .subscribe(response => {
-      console.log(response);
-    })
-  };
+    this.http
+      .post('http://localhost:3000/api/user/signup', authData)
+      .subscribe((response) => {
+        console.log(response);
+      });
+  }
 
-  login(email: string, password: string){
-    const authData: AuthData = {email: email, password: password};
+  login(email: string, password: string) {
+    const authData: AuthData = { email: email, password: password };
 
-    this.http.post<{token: string}>("http://localhost:3000/api/user/login", authData)
-    .subscribe(response => {
-      const token = response.token;
-      this.token = token;
-      this.authStatusListener.next(true);
-    })
-  };
-
+    this.http
+      .post<{ token: string }>('http://localhost:3000/api/user/login', authData)
+      .subscribe((response) => {
+        const token = response.token;
+        this.token = token;
+        if (token) {
+          this.isAuthenticated = true;
+          this.authStatusListener.next(true);
+        }
+      });
+  }
 }
