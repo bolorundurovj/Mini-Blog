1ec122f289e90ecdf00ae2a412c7e0410c943b4a
bolorundurovj <valiantjoshuabolorunduro@gmail.com>
2020-05-27 17:34:14 +0100

    Setup logout

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -49,4 +49,10 @@ export class AuthService {
         }
       });
   }
+
+  logout() {
+    this.token = null;
+    this.isAuthenticated = false;
+    this.authStatusListener.next(false);
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -23,6 +23,10 @@ export class HeaderComponent implements OnInit, OnDestroy{
     });
   }
 
+  onLogout() {
+    this.authService.logout();
+  }
+
   ngOnDestroy() {
     this.authListenerSubs.unsubscribe();
   }
