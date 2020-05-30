0f21c48e41ca97ef5755c4d19698f25163dedc1b
bolorundurovj <valiantjoshuabolorunduro@gmail.com>
2020-05-30 17:11:29 +0100

    Setup Authorization

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -34,11 +34,12 @@ export class AuthService {
 
   createUser(email: string, password: string) {
     const authData: AuthData = { email: email, password: password };
-
     this.http
       .post('http://localhost:3000/api/user/signup', authData)
       .subscribe((response) => {
         console.log(response);
+      }, (error) => {
+        console.log(error);
       });
   }
 
--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -45,7 +45,7 @@ export class PostsService {
   }
 
   getPost(id: string) {
-    return this.http.get<{ _id: string; title: string; content: string, imagePath: string }>(
+    return this.http.get<{ _id: string; title: string; content: string, imagePath: string, creator: string }>(
       'http://localhost:3000/api/posts/' + id
     );
   }
@@ -86,7 +86,8 @@ export class PostsService {
         id: id,
         title: title,
         content: content,
-        imagePath: image
+        imagePath: image,
+        creator: null
       };
     }
     this.http
@@ -98,7 +99,8 @@ export class PostsService {
           id: id,
           title: title,
           content: content,
-          imagePath: ""
+          imagePath: "",
+          creator: null
         };
         updatedPosts[oldPostIndex] = post;
         // this.posts = updatedPosts;
