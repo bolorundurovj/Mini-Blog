9cf71d8e4c3b823a26c4d8144990243a92694e78
bolorundurovj <valiantjoshuabolorunduro@gmail.com>
2020-06-01 17:35:03 +0100

    Bug Fix(edit)

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -41,7 +41,7 @@ exports.updatePost = (req, res, next) => {
   });
   Post.updateOne({ _id: req.params.id, creator: req.userData.userId }, post)
   .then((result) => {
-    if(result.nModified > 0){
+    if(result.n > 0){
       res.status(200).json({ message: "Update successful!" });
     }else {
       res.status(401).json({ message: "Not Authorized!" });
