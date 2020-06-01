0ea76b9dfca9c8a3328526ed927bb14d1a410f33
bolorundurovj <valiantjoshuabolorunduro@gmail.com>
2020-06-01 07:09:38 +0100

    Handling Auth Errors in Frontend

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -36,10 +36,10 @@ export class AuthService {
     const authData: AuthData = { email: email, password: password };
     this.http
       .post('http://localhost:3000/api/user/signup', authData)
-      .subscribe((response) => {
-        console.log(response);
-      }, (error) => {
-        console.log(error);
+      .subscribe(() => {
+        this.router.navigate(["/"]);
+      }, error => {
+        this.authStatusListener.next(false)
       });
   }
 
@@ -63,6 +63,8 @@ export class AuthService {
           this.saveAuthData(token, expirationDate, this.userId);
           this.router.navigate(['/']);
         }
+      }, error => {
+        this.authStatusListener.next(false)
       });
   }
 
