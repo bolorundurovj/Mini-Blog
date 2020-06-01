b46eadefbc3acbc08e1283c061721de5aa372970
bolorundurovj <valiantjoshuabolorunduro@gmail.com>
2020-06-01 20:48:21 +0100

    Setup GLobal Angular Config

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -5,6 +5,10 @@ import { Subject } from 'rxjs';
 
 import { AuthData } from './auth-data.model';
 
+import { environment } from 'src/environments/environment';
+
+const BACKEND_URL = environment.apiUrl + "/user/";
+
 // To provide at root level for access
 @Injectable({ providedIn: 'root' })
 export class AuthService {
@@ -35,7 +39,7 @@ export class AuthService {
   createUser(email: string, password: string) {
     const authData: AuthData = { email: email, password: password };
     this.http
-      .post('http://localhost:3000/api/user/signup', authData)
+      .post(BACKEND_URL + '/signup', authData)
       .subscribe(() => {
         this.router.navigate(["/"]);
       }, error => {
@@ -47,7 +51,7 @@ export class AuthService {
     const authData: AuthData = { email: email, password: password };
 
     this.http
-      .post<{ token: string, expiresIn: number, userId: string }>('http://localhost:3000/api/user/login', authData)
+      .post<{ token: string, expiresIn: number, userId: string }>(BACKEND_URL + '/login', authData)
       .subscribe((response) => {
         const token = response.token;
         this.token = token;
